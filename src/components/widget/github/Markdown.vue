d6e79d79c9a799133c98ce9ee761e0d65afd7464
yozman <yozman@sina.com>
2016-06-29 14:21:19 +0800

    添加笔记

--- src/components/widget/github/Markdown.vue
+++ src/components/widget/github/Markdown.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 div( :class.once=" $options.name "
-     v-html=" content | marked ")
+     v-html=" content | marked | scoped ")
 </template>
 
 <script>
@@ -10,7 +10,25 @@ export default {
     content: String
   },
   filters: {
-    marked: require('marked')
+    marked: require('marked'),
+    scoped (val) {
+      let scoped = this.$el.attributes[0].name
+      return val.replace(
+        /(<[^/>]+)([ >])/g,
+        (s, s1, s2) => `${s1} ${scoped}${s2}`
+      )
+    }
   }
 }
 </script>
+
+<style lang="sass" scoped>
+@import ~primer-markdown/lib/markdown-body
+@import ~primer-markdown/lib/headings
+@import ~primer-markdown/lib/lists
+@import ~primer-markdown/lib/tables
+@import ~primer-markdown/lib/images
+@import ~primer-markdown/lib/code
+
+.W__Github_Markdown
+  @extend .markdown-body
